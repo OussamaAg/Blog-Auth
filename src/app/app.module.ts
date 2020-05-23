9cd4d46810a406bc3e4e88164999125e30ffcfd3
Oussama_Ag <oussama.agoujil@e-polytechnique.ma>
2020-05-23 20:22:55 +0000

    Angular_part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { UserComponent } from './components/user/user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
